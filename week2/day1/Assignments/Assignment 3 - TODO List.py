3fa02f9b938b52d4a13f4759cd8a771415561adf
Hassan Noueilaty <noueilaty@gmail.com>
2017-11-20 14:32:43 -0600

    completed assignment 3

--- week2/day1/Assignments/Assignment 3 - TODO List.py
+++ week2/day1/Assignments/Assignment 3 - TODO List.py
@@ -12,28 +12,51 @@
 # - User should be able to enter priority of the task
 # - User should be able to sort the tasks based on priority
 
+print("--TODO List App--")
+
+class TODO(object):
+    def __init__(self, title):
+        self.title = title
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def view_tasks(self):
+        print('Here\'s the list of tasks:')
+        print('{}'.format(self.title))
+        counter = 1
+        for task in self.tasks:
+            print('{0}. {1}\n'.format(counter, task))
+            counter += 1
+
+    def remove_task():
+        print('Removing task')
+
+todo_list = TODO('Today\'s TODO List')
+
 while True:
     try:
-        class TODO(object):
-            def __init__(self):
-                self.tasks = []
+        print('What would you like TODO?')
 
-            def add_task(self, task):
-                self.tasks.append(task)
+    except:
+        print("Error")
 
-            def view_tasks(self):
-                print('Here\'s the list of tasks:')
-                for task in self.tasks:
-                    print('{}\n'.format(task))
-
-            def remove_task():
-                print('Removing task')
-
-            def quit():
-                print('Quitting the program')
-
-    choice = raw_input("Press q to quit \nPress any other key to continue\n> ")
-    if choice == "q":
+    print("-Press 'a' to add a task.\n-Press 'v' to view tasks.\n-Press 'r' to remove a task. \n-Press 'q' to quit.")
+    choice = raw_input('> ')
+    if choice == 'a':
+        print('Enter the task you\'d like to add')
+        task_to_add = raw_input('> ')
+        todo_list.add_task(task_to_add)
+    elif choice == 'v':
+        todo_list.view_tasks()
+    elif choice == 'r':
+        todo_list.view_tasks()
+        remove_index = raw_input('Enter the task number you want to remove >')
+        del todo_list.tasks[int(remove_index) + -1]
+        print('Deleted task #{}.'.format(remove_index))
+    elif choice == "q":
+        print('Quitting the program')
         break
-
-today = TODO()
+    else:
+        print('{} is an invalid command.'.format(choice))

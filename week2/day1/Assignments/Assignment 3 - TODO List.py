eb9805c6839174426d644c93f1b785e57dcf06ab
Hassan Noueilaty <noueilaty@gmail.com>
2017-11-20 15:14:13 -0600

    revised assignment 3

--- week2/day1/Assignments/Assignment 3 - TODO List.py
+++ week2/day1/Assignments/Assignment 3 - TODO List.py
@@ -19,7 +19,11 @@ class TODO(object):
     def add_task(self):
         print('Enter the task you\'d like to add:')
         task_to_add = raw_input('> ')
-        self.tasks.append(task_to_add)
+        is_priority = raw_input('Is this task a priortiy? (y/n) > ')
+        if is_priority == 'y':
+            self.tasks.append('! {}'.format(task_to_add))
+        else:
+            self.tasks.append(task_to_add)
 
     def view_tasks(self):
         print('Here\'s the list of tasks:')
@@ -28,19 +32,19 @@ class TODO(object):
         for task in self.tasks:
             print('{0}. {1}\n'.format(counter, task))
             counter += 1
+        print("Note: '!' denotes a priority task")
 
     def remove_task(self):
         self.view_tasks()
         remove_index = raw_input('Enter the task number you want to remove >')
-        if remove_index <= len(self.tasks):
-            del self.tasks[int(remove_index) + -1]
+        del self.tasks[int(remove_index) + -1]
         print('Deleted task #{}.'.format(remove_index))
 
 todo_list = TODO('Today\'s TODO List')
 
 while True:
     try:
-        print('What would you like TODO?')
+        print('\nWhat would you like TODO?')
 
     except:
         print("Error")

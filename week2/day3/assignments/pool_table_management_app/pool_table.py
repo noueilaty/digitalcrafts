2e39a4089cdd06629e829b4b89e3f499355e7a09
Hassan Noueilaty <noueilaty@gmail.com>
2017-11-22 19:41:54 -0600

    updated pool table management tool with JSON file

---
+++ week2/day3/assignments/pool_table_management_app/pool_table.py
@@ -0,0 +1,32 @@
+import json
+tables = tables.json
+
+print(tables)
+
+# print("""
+# -----------------------------------------------------------------------
+# Welcome to The University of Houston Pool Table Management Application:
+# -----------------------------------------------------------------------
+# """)
+
+
+
+
+#
+# class Pool_Table(object):
+#     def __init__(self):
+#
+#
+# while True:
+#     user_action = raw_input("""
+#     What would you like to do?
+#     'v' - View all tables
+#     'o' - Open a table
+#     'c' - Close a table
+#     'e' - Email report to admin
+#     'q' - Quit the program
+#     """)
+
+
+
+    # If/else conditional to confirm entry

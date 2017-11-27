039d794b977ddeb612776968722ce82b06c3b548
Hassan Noueilaty <noueilaty@gmail.com>
2017-11-27 13:17:42 -0600

    added week3 folder, notes and assignment

--- week1/day3/Assignments/calculator_helper.py
+++ week1/day3/Assignments/calculator_helper.py
@@ -1,6 +1,2 @@
 def calculate(num1, oper, num2):
     print(eval('{0} {1} {2}'.format(num1, oper, num2)))
-
-user_num1 = raw_input(print("Enter the first number you'd like to calculate > "))
-user_oper = raw_input(print("Enter the operator for your calculation > "))
-user_num2 = raw_input(print("Enter the second number you'd like to calculate > "))

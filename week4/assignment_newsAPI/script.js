ee8f59d3b03a6b2ea9017d1bd2f0f7324eb54d8c
Hassan Noueilaty <noueilaty@gmail.com>
2017-12-07 13:27:18 -0600

    initial commit of newsAPI assignment

---
+++ week4/assignment_newsAPI/script.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < news.articles.length; i++) {
+  console.log(news.articles[i]);
+
+  
+
+  `<div>
+    <h3 id="title"> <h3>
+    <h4 id="author"> </h4>
+    <h5 id="published"> </h5>
+    <p id="description"> </p>
+    <a id="URL" href=""> </a>
+  </div>`
+
+}

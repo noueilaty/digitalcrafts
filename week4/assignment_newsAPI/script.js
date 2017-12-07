543e6af823b3e7199e6f2797b18a715710a22298
Hassan Noueilaty <noueilaty@gmail.com>
2017-12-07 14:48:17 -0600

    completed newsAPI script

--- week4/assignment_newsAPI/script.js
+++ week4/assignment_newsAPI/script.js
@@ -5,11 +5,10 @@ for (var i = 0; i < news.articles.length; i++) {
 
   let newsListItem = `
   <div>
-    <img src="${news.articles[i].urlToImage}" alt="${news.articles[i].title}">
+    <img class="newsImg" src="${news.articles[i].urlToImage}" alt="${news.articles[i].title}">
     <h3><a href="${news.articles[i].url}">${news.articles[i].title}</a><h3>
     <h5>Posted ${news.articles[i].publishedAt} by ${news.articles[i].author}
     <p>${news.articles[i].description}</p>
-
   </div>`
 
   newsList.innerHTML += newsListItem;
